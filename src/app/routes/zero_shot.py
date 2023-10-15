68ed40ec1d8aeb736cd93560fac001e7c291d93b
Emir Şener <emir@cbot.ai>
2023-10-15 15:35:13 +0300

    java için

--- src/app/routes/zero_shot.py
+++ src/app/routes/zero_shot.py
@@ -8,13 +8,15 @@ json_data_path = ins_config.getDefault("json_data_path")
 zero_shot_classifier = ZeroShot(model_id, json_data_path, cache_dir)
 
 router = Blueprint("zero-shot", __name__)
+
+
 @router.route("/", methods=["POST"])
 def zero_shot():
     data = request.get_json()
-    if "sequence" in data and "labels" in data :
+    if "sequence" in data and "labels" in data:
         res = zero_shot_classifier.classify(data["sequence"], data["labels"])
         return jsonify(res)
-    else :
+    else:
         abort(400)
 
 
@@ -22,4 +24,14 @@ def zero_shot():
 def conversation():
     res = zero_shot_classifier.test_conversation()
     return jsonify(res)
-    
+
+
+@router.route("/sentiment-for-java", methods=["POST"])
+def conversation():
+    data = request.get_json()
+    labels = ["positive", "negative", "neutral"]
+    if "text" in data:
+        res = zero_shot_classifier.classify(data["text"], labels)
+        return jsonify(res)
+    else:
+        abort(400)
